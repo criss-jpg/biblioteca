f75e3649cebbb8e6d08464b7b31e482e1ced8332
criss <andersonpromadter@gmail.com>
2024-12-24 15:36:48 -0500

    primer commit

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "https://studio5.appperu.com/studio5_biblioteca/";
+const host = "138.201.200.179";
+const user = "studio5_anderson";
+const pass = "celular8899";
+const db = "studio5_biblioteca";
+const charset = "charset=utf8";
+?>
